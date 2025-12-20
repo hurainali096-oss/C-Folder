f1f8c42483112feefc2abd26096a1e8e3c20f097
Hurain Ali <anizafatima096@gmail.com>
2025-12-20 17:11:46 +0500

    Implement user info display in Programing Basics.cpp
    Added a basic program to display user information.

---
+++ Programing Basics .cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  // LET'S WE WRITE OUR NAME AND INFORMATION BY USING CODE
+cout<<"My Name is Hurain Ali";
+cout<<"I am BSCS student";
+cout<<"I am 18 year old;
+  // if you want to add more bio you write like this
+  return 0;
+}
